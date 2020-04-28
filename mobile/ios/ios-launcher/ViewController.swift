14ec5e3f027a5f5c580be71a5c72297da84497b9
Amine Rehioui <amine@franticsoftware.com>
2020-04-28 10:32:32 -0400

    iOS project

---
+++ mobile/ios/ios-launcher/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  ios-launcher
+//
+//  Created by Amine Rehioui on 2020-01-23.
+//  Copyright © 2020 Frantic Software. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKUIDelegate {
+
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        let myURL = URL(string:"http://eternadev.org")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
